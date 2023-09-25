6e1280d54d7cb78c25d99781d0842308ca927d62
whathow <marwa@gmail.com>
2023-09-25 23:13:13 +0300

    step8

--- routes/movies.js
+++ routes/movies.js
@@ -26,9 +26,9 @@ router
 .get('/get/by-title', (req,res) =>{
     res.send({ status: 200, data:[...movies].sort((a, b) => a.title.localeCompare(b.title))})
 })
-.get('/add/', (req ,res) =>{
-    res.send('create movies')
-})
+// .get('/add/', (req ,res) =>{
+//     res.send('create movies')
+// })
 .get('/edit/', (req ,res) =>{
     res.send('update movies')
 })
@@ -42,7 +42,42 @@ router
     }else{
         res.send(`{status:404, error:true, message:'the movie ${id} does not exist'}`);
     }
+      })
+
+      .get('/add/:title/:year/:rating?', (req, res) => {
+        const { title, year, rating } = req.params;
+      
+        if (!title || !year) {
+          return res.status(403).json({
+            status: 403,
+            error: true,
+            message: 'You cannot create a movie without providing a title and a year',
+          });
+        }
+      
+        if (year.length !== 4) {
+          return res.status(403).json({
+            status: 403,
+            error: true,
+            message: 'Year must be a 4-digit number',
+          });
+        }
+      
+        const movieRating = rating || 4;
+      
+        const newMovie = {
+          title,
+          year: parseInt(year),
+          rating: parseInt(movieRating),
+        };
+        movies.push(newMovie);
+       res.send(movies);
       });
+      
+      // Route to read the list of movies
+// .get('/movies/read', (req, res) => {
+//         res.json(movies);
+//       });
 
 
 
